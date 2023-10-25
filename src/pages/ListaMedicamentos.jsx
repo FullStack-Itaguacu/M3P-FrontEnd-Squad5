57acba665e4dda5e60636d941fcf97079f254d14
douglas <douglas.df1@gmail.com>
2023-10-25 12:17:40 -0300

    feat: botoes alterar pagina

--- src/pages/ListaMedicamentos.jsx
+++ src/pages/ListaMedicamentos.jsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from "react";
+import { Title, Input, Pagination } from "@mantine/core";
 // import { useNavigate } from "react-router-dom";
 import CardMedicamento from "../components/Medicamentos/cardMedicamento";
 // import { LoginContext } from "../context/LoginContext";
@@ -10,6 +11,16 @@ function ListaMedicamentos() {
   const [limit, setLimit] = useState(20);
   const [offset, setOffset] = useState(0);
   const [filter, setFilter] = useState([]);
+  const [next, setNext] = useState(false);
+  const [previous, setPrevious] = useState(false);
+
+  const setPreviousPage = () => {
+    setOffset(offset - limit);
+  };
+
+  const setNextPage = () => {
+    setOffset(offset + limit);
+  };
 
   useEffect(() => {
     const fetchData = () => {
@@ -24,6 +35,15 @@ function ListaMedicamentos() {
 
   useEffect(() => {
     console.log(listaMedicamentos);
+    if (listaMedicamentos) {
+      if (listaMedicamentos.length / limit == 1) {
+        setNext(true);
+      }
+      if (offset > 0) setPrevious(true);
+    }
+  }, [listaMedicamentos, limit]);
+
+  useEffect(() => {
     setFilter(
       listaMedicamentos.filter((el) => {
         if (barraPesquisa === "") return el;
@@ -35,17 +55,36 @@ function ListaMedicamentos() {
   return (
     <div>
       <div className="container">
-        <h3>Lista de medicamentos</h3>
-        <label htmlFor="barraPesquisa">
-          Busca
-          <input
-            type="text"
+        <Title order={1} mt="lg" ta="center">
+          Lista de medicamentos
+        </Title>
+        <Input.Wrapper label="Busca">
+          <Input
             placeholder="Digite o nome do medicamento"
-            value={barraPesquisa}
             onChange={(e) => setBarraPesquisa(e.target.value.toLowerCase())}
+            value={barraPesquisa}
           />
-        </label>
-        <p>Clique no medicamento para mais detalhes</p>
+        </Input.Wrapper>
+        <Input.Wrapper label="Medicamentos por página">
+          <Input component="select" onChange={(e) => setLimit(e.target.value)}>
+            <option value="20">20</option>
+            <option value="40">40</option>
+            <option value="60">60</option>
+            <option value="80">80</option>
+          </Input>
+        </Input.Wrapper>
+        <div>
+          {previous && (
+            <button type="button" onClick={setPreviousPage}>
+              Página anterior
+            </button>
+          )}
+          {next && (
+            <button type="button" onClick={setNextPage}>
+              Próxima página
+            </button>
+          )}
+        </div>
         <div className="grid">
           {filter.map((medicamento) => (
             <CardMedicamento medicamento={medicamento} key={medicamento.id} />
